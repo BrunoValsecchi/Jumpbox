e31b293c4be17776c1d88277d5b18da8fc7ac881
BrunoValsecchi <cai.bruno.valsecchi@gmail.com>
2023-09-22 09:26:10 -0300

    asadas
    sadasd

---
+++ src/clase10/Main.java
@@ -0,0 +1,13 @@
+package clase10;
+
+import javax.swing.JOptionPane;
+class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JOptionPane.showMessageDialog(null, "hola");
+		
+	}
+
+}
